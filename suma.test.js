a6efc5cdadd4407726b2084c25c7a80920e67e5f
jonatan <jona.cc.93@gmail.com>
2021-12-07 13:44:42 -0300

    [add]primer commit

---
+++ suma.test.js
@@ -0,0 +1,10 @@
+const { expect } = require("@jest/globals");
+const suma = require("./suma");
+
+test("2+5=7", () => {
+  expect(suma(2, 5)).toBe(7);
+});
+
+test("a+5 = NaN", () => {
+  expect(suma("a", 5)).toBe(NaN);
+});
